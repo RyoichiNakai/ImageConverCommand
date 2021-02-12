321ab883d0ec4b99d0aeb3db8339feb3e708c0b3
RyoichiNakai <elmobasketball0913@me.com>
2021-02-12 18:48:04 +0900

    add gitignore

--- main.go
+++ main.go
@@ -11,13 +11,34 @@ import (
 
 func main() {
 	var args model.Args
+	extensions := [6]string{"jpg", "jpeg", "png", "gif", "tiff", "bmp"}
 	args.BeforeExt = flag.String("b", "jpg", "Extension before conversion")
 	args.AfterExt = flag.String("a", "png", "Extension after conversion")
 	flag.Parse()
 	args.Dir = flag.Arg(0)
 
-	*args.BeforeExt = "." + *args.BeforeExt
-	*args.AfterExt = "." + *args.AfterExt
+	var (
+		before = false
+		after  = false
+	)
+
+	for _, ext := range extensions {
+		if *args.BeforeExt == ext {
+			*args.BeforeExt = "." + *args.BeforeExt
+			before = true
+		}
+
+		if *args.AfterExt == ext {
+			*args.AfterExt = "." + *args.AfterExt
+			after = true
+		}
+	}
+
+	if !before || !after {
+		fmt.Println("Extension Error:")
+		fmt.Println("You must choose image extensions.")
+		os.Exit(0)
+	}
 
 	if f, err := os.Stat(args.Dir); os.IsNotExist(err) || !f.IsDir() {
 		fmt.Fprintf(os.Stderr, "Failed to load directory:\n%v", err)
