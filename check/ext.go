90ac3abadda5172e0428b50410d139e06468faa1
RyoichiNakai <elmobasketball0913@me.com>
2021-02-20 17:19:05 +0900

    refactored

--- check/ext.go
+++ check/ext.go
@@ -6,29 +6,27 @@ import (
 )
 
 var extensions = [6]string{"jpg", "jpeg", "png", "gif", "tiff", "bmp"}
-var (
-	before = false
-	after  = false
-)
+
+// ここに宣言されると定数になり、再びこのパッケージの関数を呼び出されても値が変わらない
 
 func Ext(args model.Args) error {
-	dot := "."
+	var before bool
+	var after bool
 
 	for _, ext := range extensions {
 		if *args.BeforeExt == ext {
-			*args.BeforeExt = dot + *args.BeforeExt
+			*args.BeforeExt = "." + *args.BeforeExt
 			before = true
 		}
 
 		if *args.AfterExt == ext {
-			*args.AfterExt = dot + *args.AfterExt
+			*args.AfterExt = "." + *args.AfterExt
 			after = true
 		}
 	}
 
 	if !before || !after {
-		err := errors.New("Extension Error: You must choose image extensions.")
-		return err
+		return errors.New("Extension Error: You must choose image extensions")
 	}
 
 	return nil
